051dcecf332c80717d157dad48b76a895b41e063
hiro.bata <kawa.060313@gmail.com>
2020-08-22 10:00:35 +0000

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(id: :desc).page(params[:page])
+  end
+  
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create, :show]
   resources :posts, only: [:index, :new, :create, :destroy]
 end
