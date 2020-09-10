3a74c2bb238b0d7ef4a6e11a97693cfd77d5f013
hiro.bata <kawa.060313@gmail.com>
2020-09-10 08:20:03 +0000

    admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:show]
+  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :user_admin, only: [:index]
+  
+  def index
+  end
   
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.order(id: :desc).page(params[:page])
-    @favorites = current_user.favorite_posts.includes(:user)
+      @user = User.find(params[:id])
+      @posts = @user.posts.order(id: :desc).page(params[:page])
+      @favorites = current_user.favorite_posts.includes(:user)
   end
   
   def new
@@ -22,17 +26,26 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    flash[:success] = "ユーザーを削除しました。"
+    redirect_back(fallback_location: root_path)
+  end
+  
   private
   
   def user_params 
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  def correct_user
-    @post = current_user.posts.find_by(id: params[:id])
-    unless @post
-      redirect_to root_url  
-    end
+  def user_admin
+    @users = User.all
+      if  current_user.admin == false
+          redirect_to root_path
+      else
+          render action: "index"
+      end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
-  resources :users, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show] do
+    delete :destroy, on: :member
+  end
   
   resources :posts, only: [:index, :new, :create, :destroy] do
     get :search, on: :collection
