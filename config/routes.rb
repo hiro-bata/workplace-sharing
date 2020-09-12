85eebb452c6c3995a652200ad04b807870e402b9
hiro.bata <kawa.060313@gmail.com>
2020-09-12 06:03:45 +0000

    users/likes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
+  # get "likes", to: "users#likes"
   resources :users, only: [:index, :new, :create, :show] do
     delete :destroy, on: :member
+    get :likes, on: :member
   end
   
   resources :posts, only: [:index, :new, :create, :destroy] do
